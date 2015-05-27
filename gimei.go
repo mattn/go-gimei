44d250674257f84b75073a1efa2aeb6fc47ecf76
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2015-05-27 09:02:55 +0900

    doc

--- gimei.go
+++ gimei.go
@@ -24,31 +24,50 @@ func init() {
 	r = rand.New(rand.NewSource(time.Now().UnixNano()))
 }
 
+// Item take three figure for japanese. Kanji/Hiragana/Katakana.
+// It's not only the difference of sounds, it just letters.
 type Item []string
 
+// String implement Stringer
 func (i Item) String() string {
 	return i.Kanji()
 }
 
+// Kanji return string of Item as kanji.
 func (i Item) Kanji() string {
 	return i[0]
 }
 
+// Hiragana return string of Item as hiragana.
 func (i Item) Hiragana() string {
 	return i[1]
 }
 
+// Hiragana return string of Item as katakana.
 func (i Item) Katakana() string {
 	return i[2]
 }
 
+// Sex store Male ore Female.
 type Sex int
 
+// String implement Stringer.
+func (s Sex) String() string {
+	switch s {
+	case Male:
+		return "男"
+	case Female:
+		return "女"
+	}
+	panic("wtf!?")
+}
+
 const (
-	Male Sex = iota
-	Female
+	Male   Sex = iota // 男
+	Female            // 女
 )
 
+// name store data sturecture just same as names.yml.
 type name struct {
 	FirstName struct {
 		Male   []Item `yaml:"male"`
@@ -57,6 +76,7 @@ type name struct {
 	LastName []Item `yaml:"last_name"`
 }
 
+// Name store name and sex for a person.
 type Name struct {
 	First Item
 	Last  Item
@@ -78,30 +98,37 @@ func loadNames() {
 	panic("failed to load names data")
 }
 
+// String implement Stringer.
 func (n *Name) String() string {
 	return n.Kanji()
 }
 
+// Kanji return string of Name as kanji.
 func (n *Name) Kanji() string {
 	return n.Last.Kanji() + " " + n.First.Kanji()
 }
 
+// Hiragana return string of Name as hiragana.
 func (n *Name) Hiragana() string {
 	return n.Last.Hiragana() + " " + n.First.Hiragana()
 }
 
+// Hiragana return string of Name as katakana.
 func (n *Name) Katakana() string {
 	return n.Last.Katakana() + " " + n.First.Katakana()
 }
 
+// IsMale return true if he is male.
 func (n *Name) IsMale() bool {
 	return n.Sex == Male
 }
 
+// IsMale return true if she is female.
 func (n *Name) IsFemale() bool {
 	return n.Sex == Female
 }
 
+// NewName return new instance of person.
 func NewName() *Name {
 	if rand.Int()%2 == 0 {
 		return NewMale()
@@ -110,6 +137,7 @@ func NewName() *Name {
 	}
 }
 
+// NewMale return new instance of person that is male.
 func NewMale() *Name {
 	onceName.Do(loadNames)
 	return &Name{
@@ -119,6 +147,7 @@ func NewMale() *Name {
 	}
 }
 
+// NewFemale return new instance of person that is female.
 func NewFemale() *Name {
 	onceName.Do(loadNames)
 	return &Name{
@@ -162,18 +191,22 @@ func findNameByIndex(n string, i int) *Name {
 	return nil
 }
 
-func FindNameByKanji(name string) *Name {
-	return findNameByIndex(name, 0)
+// FindNameByKanji find Name from kanji.
+func FindNameByKanji(kanji string) *Name {
+	return findNameByIndex(kanji, 0)
 }
 
-func FindNameByHiragana(name string) *Name {
-	return findNameByIndex(name, 1)
+// FindNameByHiragana find Name from hiragana.
+func FindNameByHiragana(hiragana string) *Name {
+	return findNameByIndex(hiragana, 1)
 }
 
-func FindNameByKatakana(name string) *Name {
-	return findNameByIndex(name, 2)
+// FindNameByKanji find Name from katakana.
+func FindNameByKatakana(katakana string) *Name {
+	return findNameByIndex(katakana, 2)
 }
 
+// address store data sturecture just same as addresses.yml.
 type address struct {
 	Addresses struct {
 		Prefecture []Item `yaml:"prefecture"`
@@ -182,6 +215,7 @@ type address struct {
 	} `yaml:"addresses"`
 }
 
+// Address store address that is pointed by prefecture/city/town.
 type Address struct {
 	Prefecture Item
 	City       Item
@@ -203,22 +237,27 @@ func loadAddresses() {
 	panic("failed to load names data")
 }
 
+// String implement Stringer.
 func (a *Address) String() string {
 	return a.Kanji()
 }
 
+// Kanji return string of Address as kanji.
 func (a *Address) Kanji() string {
 	return a.Prefecture.Kanji() + a.City.Kanji() + a.Town.Kanji()
 }
 
+// Hiragana return string of Address as hiragana.
 func (a *Address) Hiragana() string {
 	return a.Prefecture.Hiragana() + a.City.Hiragana() + a.Town.Hiragana()
 }
 
+// Hiragana return string of Address as katakana.
 func (a *Address) Katakana() string {
 	return a.Prefecture.Katakana() + a.City.Katakana() + a.Town.Katakana()
 }
 
+// NewAddress return new instance of address.
 func NewAddress() *Address {
 	return &Address{
 		Prefecture: NewPrefecture(),
@@ -227,16 +266,19 @@ func NewAddress() *Address {
 	}
 }
 
+// NewPrefecture return new instance of prefecture.
 func NewPrefecture() Item {
 	onceAddress.Do(loadAddresses)
 	return addresses.Addresses.Prefecture[r.Int()%len(addresses.Addresses.Prefecture)]
 }
 
+// NewTown return new instance of town.
 func NewTown() Item {
 	onceAddress.Do(loadAddresses)
 	return addresses.Addresses.Town[r.Int()%len(addresses.Addresses.Town)]
 }
 
+// NewCity return new instance of city.
 func NewCity() Item {
 	onceAddress.Do(loadAddresses)
 	return addresses.Addresses.City[r.Int()%len(addresses.Addresses.City)]
@@ -256,11 +298,6 @@ func findAddressByIndex(a string, i int) *Address {
 				if a != prefecture[i]+city[i]+town[i] {
 					continue
 				}
-				println((&Address{
-					Prefecture: prefecture,
-					City:       city,
-					Town:       town,
-				}).String())
 				return &Address{
 					Prefecture: prefecture,
 					City:       city,
@@ -272,14 +309,17 @@ func findAddressByIndex(a string, i int) *Address {
 	return nil
 }
 
-func FindAddressByKanji(address string) *Address {
-	return findAddressByIndex(address, 0)
+// FindAddressByKanji find Address from kanji.
+func FindAddressByKanji(kanji string) *Address {
+	return findAddressByIndex(kanji, 0)
 }
 
-func FindAddressByHiragana(address string) *Address {
-	return findAddressByIndex(address, 1)
+// FindAddressByHiragana find Address from hiragana.
+func FindAddressByHiragana(hiragana string) *Address {
+	return findAddressByIndex(hiragana, 1)
 }
 
-func FindAddressByKatakana(address string) *Address {
-	return findAddressByIndex(address, 2)
+// FindAddressByKanji find Address from katakana.
+func FindAddressByKatakana(katakana string) *Address {
+	return findAddressByIndex(katakana, 2)
 }
